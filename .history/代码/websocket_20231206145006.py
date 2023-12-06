717991f0a9a72d58fb45bd8eb4fa4f4da17cfd9d
ct5ctl <1079314004@qq.com>
2023-12-06 14:50:06 +0800

    u

---
+++ .history/代码/websocket_20231206145006.py
@@ -0,0 +1,38 @@
+import asyncio
+import json
+import websockets
+import pos_server
+
+async def send_message(websocket):
+    while True:
+        # 创建你想要发送的消息
+        pos_current = pos_server.vehicle_data.get_pos_current()
+        theta_current = pos_server.vehicle_data.get_theta_current()
+        message = {
+            "eventName": "eventValue",
+            "data": f'[[{pos_current[0]},{pos_current[1]},{pos_current[2]}]]'
+        }
+
+        # 发送消息给客户端
+        # await websocket.send(str(message))
+        await websocket.send(json.dumps(message))
+
+        # 等待一段时间再发送下一条消息
+        await asyncio.sleep(0.1)  # 100ms
+
+async def echo(websocket, path):
+    # 启动发送消息的任务
+    send_task = asyncio.create_task(send_message(websocket))
+
+    try:
+        # 防止任务提前结束
+        await send_task
+    except asyncio.CancelledError:
+        pass
+
+# 设置服务器 IP 地址和端口号
+start_server = websockets.serve(echo, "127.0.0.1", 8765)
+
+# 启动服务器
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
