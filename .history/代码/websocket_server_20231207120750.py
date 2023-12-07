f4a221eb6d9900927acddca9b4b71a3c8a366e2e
ct5ctl <1079314004@qq.com>
2023-12-07 12:07:54 +0800

    u

--- .history/代码/websocket_server_20231207120750.py
+++ .history/代码/websocket_server_20231207120750.py
@@ -54,7 +54,7 @@ async def echo(websocket, path):
 
 # 设置服务器 IP 地址和端口号
 # start_server = websockets.serve(echo, "127.0.0.1", 18765)
-start_server = websockets.serve(echo, "192.168.229.125", 61111)
+# start_server = websockets.serve(echo, "192.168.229.125", 61111)
 
 
 # 启动服务器
