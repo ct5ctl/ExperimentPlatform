ef41860f37f74c1e5ff955e1fd332febc9a7c267
ct5ctl <1079314004@qq.com>
2023-12-19 13:06:47 +0800

    u

--- .history/代码/websocket_client_test_20231219130640.py
+++ .history/代码/websocket_client_test_20231219130640.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def receive_message():
-    # uri = "ws://127.0.0.1:18666"  # 修改为你的服务器地址和端口
+    uri = "ws://127.0.0.1:18666"  # 修改为你的服务器地址和端口
     uri = "ws://192.168.8.125:9876"  # 修改为你的服务器地址和端口
 
     async with websockets.connect(uri) as websocket:
