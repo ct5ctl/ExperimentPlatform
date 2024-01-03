957fb7e3bd988ced24b052e6d2d3dc30c53a1d98
ct5ctl <1079314004@qq.com>
2024-01-03 15:05:20 +0800

    u

--- .history/代码/experiment_platform_server_20240103134906.py
+++ .history/代码/experiment_platform_server_20240103134906.py
@@ -416,9 +416,9 @@ def start_websocket_server(q_pos):
             pass
 
     # 启动 WebSocket 服务器
-    # start_server = websockets.serve(echo, "192.168.229.125", 9876)
+    start_server = websockets.serve(echo, "192.168.229.125", 9876)
     # start_server = websockets.serve(echo, "192.168.8.125", 9876)
-    start_server = websockets.serve(echo, "127.0.0.1", 9876)
+    # start_server = websockets.serve(echo, "127.0.0.1", 9876)
     asyncio.get_event_loop().run_until_complete(start_server)
     print("Server started")
     asyncio.get_event_loop().run_forever()
@@ -464,11 +464,11 @@ if __name__ == "__main__":
     websocket_server_process = multiprocessing.Process(target=start_websocket_server, args=(q_pos, ))   # 参数的逗号不能省略！否则会被判断为一个对象而非元组
     websocket_server_process.start()
 
-    flag = multiprocessing.Event()
-    flag.clear()  
-    # 启动导航模拟报文发送进程
-    navigation_simulation_process = multiprocessing.Process(target=navigation_simulation_server, args=(q_pos, q_theta, flag, simula_data))
-    navigation_simulation_process.start()
+    # flag = multiprocessing.Event()
+    # flag.clear()  
+    # # 启动导航模拟报文发送进程
+    # navigation_simulation_process = multiprocessing.Process(target=navigation_simulation_server, args=(q_pos, q_theta, flag, simula_data))
+    # navigation_simulation_process.start()
     
     
 
